f19b1f0fad23d0c6bf47dc79b57257167b9e3d89
Данила Морозов <89854821428@yandex.ru>
2019-12-15 12:01:57 +0300

    Modal-переделать

--- src/main/resources/static/js/allFilm.js
+++ src/main/resources/static/js/allFilm.js
@@ -33,8 +33,38 @@ $(document).ready(function() {
                     "<td>" + film[i].title +"</td>"+
                     "<td>" + film[i].rating +"</td>"+
                     "<td>" +film[i].age + "</td>"+
-                    " <td>" +
-                    "<a class='\btn btn-primary' href=\"editFilm?id=" + film[i].id + "\">Edit</a>" +
+                    "<td>" +
+                    "<a type=\"button\" class=\"btn btn-primary\" data-toggle=\"modal\" data-target=\"#exampleModal\">\n" +
+                    "  Edit\n" +
+                    "</a>\n" +
+                    "\n" +
+                    "<div class=\"modal fade\" id=\"exampleModal\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"exampleModalLabel\" aria-hidden=\"true\">\n" +
+                    "  <div class=\"modal-dialog\" role=\"document\">\n" +
+                    "    <div class=\"modal-content\">\n" +
+                    "      <div class=\"modal-header\">\n" +
+                    "        <h5 class=\"modal-title\" id=\"exampleModalLabel\">Modal title</h5>\n" +
+                    "        <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\">\n" +
+                    "          <span aria-hidden=\"true\">&times;</span>\n" +
+                    "        </button>\n" +
+                    "      </div>\n" +
+                    "      <div class=\"modal-body\">\n" +
+                    "        <form action=\"/editFilm\" method=\"post\">\n" +
+                    "        <input name=\"id\" readonly>\n" +
+                    "        <h3 id=\"Title\">Title</h3>\n" +
+                    "        <input name=\"title\" \"залупа\">\n" + "\n" +
+                    "        <h3 id=\"Rat\">Rat</h3>\n" +
+                    "        <input name=\"rat\" \"залупа\">\n" + "\n" +
+                    "        <h3 id=\"age\">Age</h3>\n" +
+                    "        <input name=\"age\" \"залупа\">\n" + "   \n" +
+                    "      </div>\n" +
+                    "      <div class=\"modal-footer\">\n" +
+                    "        <button type=\"button\" class=\"btn btn-secondary\" data-dismiss=\"modal\">Close</button>\n" +
+                    "        <button type=\"submit\" class=\"btn btn-success\">Save changes</button>\n" +
+                    "      </div>\n" +
+                    "      </form>\n" +
+                    "    </div>\n" +
+                    "  </div>\n" +
+                    "</div>" +
                     "<a class='\btn btn-danger' href=\"deleteFilm?id=" + film[i].id + "\">Delete</a>" +
                     "</td>"+
                     "</tr>");
@@ -47,3 +77,9 @@ $(document).ready(function() {
         }
     });
 });
+
+/*
+
+
+
+* */
